156265dd48a407b87fd5d13409c488fd7597a26e
bee_olly <134434470+beolly@users.noreply.github.com>
2025-11-15 10:47:38 +0200

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function isEnoughCapacity(products, containerSize) {
+  const values = Object.values(products);
+  let total = 0;
+  for (let amount of values) {
+    total += amount;
+  }
+  return total <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); 
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); 
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
